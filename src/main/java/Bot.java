d7dc006c9e07eba578de1b2fe38300ad09e0faf7
UnderscoreLodii <krzak.dan@gmail.com>
2026-02-24 17:30:36 +0100

    First command

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,15 +1,37 @@
-import io.github.cdimascio.dotenv.DotenvEntry;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import java.util.Set;
+public class Bot extends ListenerAdapter {
 
-public class Bot {
-    private static String getToken(){
-        return Dotenv.load().get("TOKEN");
+    private static String getToken(){return Dotenv.load().get("TOKEN");}
+
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault(getToken()).addEventListeners(new Bot()).build();
+
+        CommandListUpdateAction commands = jda.updateCommands();
+
+        commands.addCommands(Commands.slash("greet", "Sends a hi message, pinging user that sent that message"));
+
+        commands.queue();
     }
-    static void main() {
-        JDA api = JDABuilder.createDefault(getToken()).build();
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
+        switch(event.getName()){
+            case "greet":
+                Member member = event.getMember();
+                greetMember(event, member);
+                break;
+        }
+    }
+
+    public void greetMember(SlashCommandInteractionEvent event, Member member){
+        event.reply("Hi " + member.getAsMention()).queue();
     }
 }
