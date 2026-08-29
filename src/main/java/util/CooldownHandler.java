d202f9bee45c6d82f44e7eec4a4dc7801d74d1fb
UnderscoreLodii <krzak.dan@gmail.com>
2026-08-29 18:44:52 +0200

    Changed the logic of Cooldown handler to use cache instead of map and a timer

--- src/main/java/util/CooldownHandler.java
+++ src/main/java/util/CooldownHandler.java
@@ -1,59 +1,26 @@
 package util;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 public class CooldownHandler {
-
-    private final Map<Long, Map<Long, Timer>> timers = new HashMap<>();
+    private final Cache<CooldownKey, Boolean> timers;
     private final Long standardCooldownTime;
 
     public CooldownHandler(Long cooldownTime) {
         this.standardCooldownTime = cooldownTime;
+        this.timers = Caffeine.newBuilder()
+                .expireAfterWrite(standardCooldownTime, java.util.concurrent.TimeUnit.MILLISECONDS)
+                .build();
     }
 
     public boolean checkCooldown(Long guildId, Long userId) {
-        return timers.getOrDefault(guildId, new HashMap<>()).getOrDefault(userId, Timer.ALWAYS_READY).isReady();
+        return timers.getIfPresent(new CooldownKey(guildId, userId)) == null;
     }
 
     public void resetCooldown(Long guildId, Long userId) {
-        timers.computeIfAbsent(guildId, _ -> new HashMap<>())
-                .compute(userId, (_, existingTimer) -> {
-                    if (existingTimer != null && existingTimer != Timer.ALWAYS_READY) {
-                        existingTimer.startTimer(standardCooldownTime);
-                        return existingTimer;
-                    }
-                    return Timer.buildTheTimerAndStart(standardCooldownTime);
-                });
+        timers.put(new CooldownKey(guildId, userId), Boolean.TRUE);
     }
 
-    private static class Timer {
-
-        public static final Timer ALWAYS_READY = new Timer() {
-            @Override
-            public boolean isReady() {
-                return true;
-            }
-            @Override
-            public void startTimer(long timeInMillis) {
-                throw new UnsupportedOperationException("Cannot restart the readyTimer instance.");
-            }
-        };
-
-        private long readyTime = 0;
-
-        public static Timer buildTheTimerAndStart(long timeInMillis) {
-            Timer timer = new Timer();
-            timer.startTimer(timeInMillis);
-            return timer;
-        }
-
-        public boolean isReady() {
-            return readyTime < System.currentTimeMillis();
-        }
-
-        public void startTimer(long timeInMillis) {
-            readyTime = System.currentTimeMillis() + timeInMillis;
-        }
-    }
+    private record CooldownKey(Long guildId, Long userId) {}
 }
